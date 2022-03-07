e0a2011ec22620992a38e951b723ead349a1e7ae
Lucky7Tb <Luckytri101@gmail.com>
2022-03-08 01:27:46 +0700

    update: database

---
+++ database/migrations/2022_03_07_012045_alter_lesson_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lesson_subjects', function (Blueprint $table) {
+            $table->addColumn('string', 'thumbnail')
+                ->length(100)
+                ->nullable()
+                ->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lesson_subjects', function (Blueprint $table) {
+            $table->dropColumn('thumbnail');
+        });
+    }
+};
