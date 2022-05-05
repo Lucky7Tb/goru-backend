5a869cf8c111ec0f8c517d345920027ca6ae57ac
Lucky7Tb <Luckytri101@gmail.com>
2022-05-06 01:02:52 +0700

    update cors config and env

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'goru.test'],
+    'allowed_origins' => explode(",", env('ALLOWED_ORIGIN')),
 
     'allowed_origins_patterns' => [],
 
